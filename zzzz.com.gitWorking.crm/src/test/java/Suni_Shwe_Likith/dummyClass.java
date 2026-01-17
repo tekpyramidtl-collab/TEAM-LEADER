8f46e65d521f3545d9c7e636ce201f7bd3c09aa1
Shwetha <Shwetha@LAPTOP-E28IQ4TJ>
2026-01-17 13:26:35 +0530

    Name_Element

--- zzzz.com.gitWorking.crm/src/test/java/Suni_Shwe_Likith/dummyClass.java
+++ zzzz.com.gitWorking.crm/src/test/java/Suni_Shwe_Likith/dummyClass.java
@@ -1,10 +1,23 @@
 package Suni_Shwe_Likith;
 
-public class dummyClass {
-public static void main(String[] args) {
-	
-System.out.println();
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class dummyClass {
+public static void main(String[] args) throws InterruptedException {
+	
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://demoapps.qspiders.com/ui?scenario=1");
+	
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+	driver.findElement(By.id("name")).sendKeys("Shwetha");
+	Thread.sleep(2000);
+	driver.close();
 }
 }
 
